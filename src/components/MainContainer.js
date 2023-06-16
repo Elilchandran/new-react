59c709168eb666cc19e3e268f69bb39eed506db1
Elilarasi <127470628+Elilchandran@users.noreply.github.com>
2023-06-16 19:25:29 +0530

    Update MainContainer.js

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React from 'react'
 
 export default function MainContainer() {
-  const backgroundImage = "url('src/Rimg.png')";
-  const backgroundStyle = {
-    backgroundImage: backgroundImage,
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'right 85%',
-  };
-
-  return (
-    <main style={backgroundStyle}>
-      <h1 className="h1-item">Fun facts about React</h1>
-      <ul className="ul-item">
-        <li>It's a popular library, so I'll be able to play in the cool world!</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-      </ul>
-    </main>
-  );
-}
-
+    return (
+      <main>
+        <h1 className="h1-item">Fun facts about React</h1>
+        <ul className="ul-item">
+          <li>It's a popular library, so I'll be able to play in the cool world!</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+          <li>I'm more likely to get a job as a developer if I know React</li>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+        </ul>
+      </main>
+    );
+  }
 
